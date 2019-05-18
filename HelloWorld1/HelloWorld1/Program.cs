f9fd992b66f190a9b7e5a4779d627a600614a9d5
Jfleming07712 <50762378+Jfleming07712@users.noreply.github.com>
2019-05-18 18:56:36 -0400

    Hello world program with Bretts tutalige (#2)
    * Hello world program with Bretts tutalige
    
    * Changed

---
+++ HelloWorld1/HelloWorld1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++) {
+                string NumberAsSTring = TurnNumberIntoString(i);
+                Console.WriteLine(NumberAsSTring);
+            }
+
+            ////int[] things = new int[]{ 1, 2, 3 };
+            List<int> things = new List<int> { 1, 2, 3 };
+
+            foreach (int thing in things)
+            {
+                string val = TurnNumberIntoString(thing);
+                Console.WriteLine(val);
+            }
+        }
+
+        public static string TurnNumberIntoString(int aNumber)
+        {
+            int blah = aNumber * 1000 + 150;
+            string returnValue = $"the value of {nameof(blah)} is {blah}";
+            return returnValue;
+        }
+    }
+}
